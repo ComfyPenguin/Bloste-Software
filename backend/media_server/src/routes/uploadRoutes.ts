bb3feb9c939d96413d4ce590490079d09df00430
Andy Pérez <andypr97vlc@gmail.com>
2026-01-12 12:02:52 +0100

    Quitada extension a la hora de servir hls - Anteriormente: /api/hls/{id_video}/master.m3u8 - Actualmente: /api/hls/{id_video}

--- backend/media_server/src/routes/uploadRoutes.ts
+++ backend/media_server/src/routes/uploadRoutes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import express from "express";
+import path from "path";
+import fs from "fs-extra";
 import { uploadVideo, getVideoMetadata, getAllVideos } from "../controllers/videoController";
 import storageConfig from "../config/storage.config";
 
@@ -14,7 +15,16 @@ router.get("/videos", getAllVideos);
 // GET /api/videos/:id - Obtener metadatos de un vídeo
 router.get("/videos/:id", getVideoMetadata);
 
-// Servir archivos HLS
-router.use("/hls", express.static(storageConfig.publicHlsDir));
+// GET /api/hls/:id - Servir video
+router.get("/hls/:id", async (req, res) => {
+  const masterPath = path.resolve(storageConfig.publicHlsDir, req.params.id, "master.m3u8");
+
+  // TODO: Mejorar la gestión de errores (Middleware de errores global)
+  if (!(await fs.pathExists(masterPath))) {
+    return res.status(404).json({ error: "HLS master not found" });
+  }
+
+  res.sendFile(masterPath);
+});
 
 export default router;
