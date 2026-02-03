5029f8b1d24d5032a1b6e439ba2280f5aa01f00e
rubenTalora <rubentalora06@gmail.com>
2026-02-03 09:15:55 +0100

    Squashed commit of the following:
    commit 6ddff033be470bba865c9cc775abba394c638554
    Merge: 0724ea6 645c7e4
    Author: rubenTalora <rubentalora06@gmail.com>
    Date:   Tue Feb 3 09:13:01 2026 +0100
    
        Merge branch 'separara-modulos' of https://github.com/ComfyPenguin/Bloste-Software into separara-modulos
    
    commit 0724ea61170012d4074ea312f38c187a5c05f6a0
    Author: rubenTalora <rubentalora06@gmail.com>
    Date:   Tue Feb 3 09:04:37 2026 +0100
    
        Revert "Merge branch 'separara-modulos' of https://github.com/ComfyPenguin/Bloste-Software into separara-modulos"
    
        This reverts commit a0361a21bfd8b3dfe988bf7a9c105ba0fb61e0c9, reversing
        changes made to fbe6f5ef4f3c7e086dc88e2c8715f18c418ac083.
    
    commit eff3204c2a7b58eb978c1069fbdbf2d0cd061b39
    Author: rubenTalora <rubentalora06@gmail.com>
    Date:   Tue Feb 3 08:57:07 2026 +0100
    
        reparacion error al instalar modulo usuarios
    
    commit 6964ecc974cb23bc4ea1cf883544d044dd5a67fb
    Author: rubenTalora <rubentalora06@gmail.com>
    Date:   Fri Jan 30 12:18:56 2026 +0100
    
        estructura modulo web
    
    commit 645c7e4b74f0978b7c9f8f9b844c502960dff21d
    Author: rubenTalora <rubentalora06@gmail.com>
    Date:   Tue Feb 3 09:04:37 2026 +0100
    
        Revert "Merge branch 'separara-modulos' of https://github.com/ComfyPenguin/Bloste-Software into separara-modulos"
    
        This reverts commit a0361a21bfd8b3dfe988bf7a9c105ba0fb61e0c9, reversing
        changes made to fbe6f5ef4f3c7e086dc88e2c8715f18c418ac083.
    
    commit a0361a21bfd8b3dfe988bf7a9c105ba0fb61e0c9
    Merge: fbe6f5e 497673c
    Author: rubenTalora <rubentalora06@gmail.com>
    Date:   Tue Feb 3 08:58:35 2026 +0100
    
        Merge branch 'separara-modulos' of https://github.com/ComfyPenguin/Bloste-Software into separara-modulos
    
    commit fbe6f5ef4f3c7e086dc88e2c8715f18c418ac083
    Author: rubenTalora <rubentalora06@gmail.com>
    Date:   Tue Feb 3 08:57:07 2026 +0100
    
        reparacion error al instalar modulo usuarios
    
    commit ce44a7d668cb211fba2f652dd4925e963a687745
    Author: rubenTalora <rubentalora06@gmail.com>
    Date:   Mon Feb 2 13:02:48 2026 +0100
    
        reparacion bd bloste website
    
    commit fc9a99f91a1d3594eac86a70cd2839e3d56cb3ef
    Author: rubenTalora <rubentalora06@gmail.com>
    Date:   Fri Jan 30 12:18:56 2026 +0100
    
        estructura modulo web
    
    commit 497673c2a38af1be4a24ac41884d784b4185c7b9
    Author: rubenTalora <rubentalora06@gmail.com>
    Date:   Tue Feb 3 08:57:07 2026 +0100
    
        reparacion error al instalar modulo usuarios
    
    commit 720790d6c63604cc73117f421d41779a13fb5803
    Author: rubenTalora <rubentalora06@gmail.com>
    Date:   Mon Feb 2 13:02:48 2026 +0100
    
        reparacion bd bloste website
    
    commit 9a56fd7e4b41c7741ffcde5ba29b13f09803add5
    Author: rubenTalora <rubentalora06@gmail.com>
    Date:   Fri Jan 30 12:18:56 2026 +0100
    
        estructura modulo web

--- backend/odoo_mod/Bloste_core/models/res_users.py
+++ backend/odoo_mod/Bloste_core/models/res_users.py
@@ -11,34 +11,67 @@ class ResUsers(models.Model):
         ('admin', 'Administrador')
     ], string='Estado en BlosteFlix', default='user')
     is_admin_bloste = fields.Boolean(string='¿Es Admin en BlosteFlix?', default=False)
+    plain_password = fields.Char(string='Contraseña', help='Contraseña en texto plano')
     
     # Campos para mostrar la suscripción activa actual
-    active_subscription_id = fields.Many2one('bloste.subscription', string='Suscripción Activa', compute='_compute_active_subscription', readonly=True)
-    subscription_expiry_date = fields.Date(string='Caducidad de Suscripción', compute='_compute_subscription_expiry', readonly=True)
+    active_subscription_id = fields.Many2one('bloste.subscription', string='Suscripción Activa', compute='_compute_active_subscription', store=True, readonly=True)
+    subscription_expiry_date = fields.Date(string='Caducidad de Suscripción', compute='_compute_subscription_expiry', store=True, readonly=True)
     
-    @api.depends('user_subscription_ids', 'user_subscription_ids.end_date')
-    def _compute_active_subscription(self):
-        """Obtiene la suscripción activa actual (la que no ha caducado)"""
-        today = fields.Date.today()
-        for record in self:
-            active_sub = None
-            for user_sub in record.user_subscription_ids:
-                # Buscar la suscripción que aún no ha caducado
-                if user_sub.end_date and user_sub.end_date >= today:
-                    active_sub = user_sub.subscription_id
-                    break
-            record.active_subscription_id = active_sub
+    # Campo auxiliar para asignar nueva suscripción desde formulario
+    new_subscription_id = fields.Many2one('bloste.subscription', string='Asignar Suscripción')
+    active_subscription_id = fields.Many2one('bloste.subscription', string='Suscripción Activa', compute='_compute_active_subscription', store=True)
+    subscription_expiry_date = fields.Date(string='Caducidad de Suscripción', compute='_compute_subscription_expiry', store=True, readonly=True)
     
-    @api.depends('user_subscription_ids', 'user_subscription_ids.end_date')
+    # Campo auxiliar para asignar nueva suscripción desde formulario
+    new_subscription_id = fields.Many2one('bloste.subscription', string='Asignar Suscripción')
+
+    @api.depends('user_subscription_ids.end_date')
     def _compute_subscription_expiry(self):
         """Obtiene la fecha de caducidad de la suscripción activa"""
         today = fields.Date.today()
         for record in self:
             expiry_date = None
-            for user_sub in record.user_subscription_ids:
-                # Buscar la suscripción que aún no ha caducado
-                if user_sub.end_date and user_sub.end_date >= today:
-                    expiry_date = user_sub.end_date
-                    break
+            # Buscar la suscripción que aún no ha caducado y devolver la más próxima
+            valid_subs = record.user_subscription_ids.filtered(lambda s: s.end_date and s.end_date >= today)
+            if valid_subs:
+                # ordenar por end_date ascendente y tomar la primera
+                expiry_date = sorted(valid_subs, key=lambda s: s.end_date)[0].end_date
             record.subscription_expiry_date = expiry_date
+
+    @api.depends('user_subscription_ids.end_date')
+    def _compute_active_subscription(self):
+        """Establece `active_subscription_id` como la suscripción con fecha de caducidad más próxima (no caducada)."""
+        today = fields.Date.today()
+        for record in self:
+            active_sub = None
+            valid_subs = record.user_subscription_ids.filtered(lambda s: s.end_date and s.end_date >= today)
+            if valid_subs:
+                # escoger la suscripción con la fecha de caducidad más cercana
+                active_sub = sorted(valid_subs, key=lambda s: s.end_date)[0].subscription_id
+            record.active_subscription_id = active_sub
+
+    def write(self, vals):
+        """Si se cambia `new_subscription_id` creamos una nueva entrada en bloste.user_subscription
+        con fecha de inicio hoy. Mantener historial de suscripciones.
+        Si se cambia `password`, guardamos también en plain_password."""
+        # Sincronizar password con plain_password
+        if 'password' in vals:
+            vals['plain_password'] = vals['password']
+        
+        res = super(ResUsers, self).write(vals)
+        
+        if 'new_subscription_id' in vals:
+            sub_id = vals.get('new_subscription_id')
+            if sub_id:
+                today = fields.Date.today()
+                for user in self:
+                    # crear nueva suscripción de usuario
+                    self.env['bloste.user_subscription'].create({
+                        'user_id': user.id,
+                        'subscription_id': int(sub_id),
+                        'start_date': today,
+                    })
+                    # Limpiar el campo auxiliar
+                    user.new_subscription_id = False
+        return res
 
